7a2912889d2933d68dbf8c2e4ebfdfd8db64aaf1
shawn <shawnpatel196@gmail.com>
2020-01-17 12:21:08 -0600

    got some of stretch done, but not working

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -8,6 +8,8 @@
 */
 
 /* HTML:
+<div class="carousel-container">
+  
   <div class="carousel">
     <div class="left-button"> < </div>
     <img src="./assets/carousel/mountains.jpeg" />
@@ -16,4 +18,63 @@
     <img src="./assets/carousel/turntable.jpeg" />
     <div class="right-button"> > </div>
   </div>
+  </div>
+  
 */
+
+
+function carouselCreator(){
+  // Creating Elements
+  const carousel=document.createElement('div');
+  const leftButton= document.createElement('div');
+  const mountainsImage= document.createElement('img');
+  const computerImage=document.createElement('img');
+  const treesImage=document.createElement('img');
+  const turnTableImage=document.createElement('img');
+  const rightButton=document.createElement('div');
+
+  // Appendiung CHildren to parent
+  carousel.append(leftButton);
+  carousel.append(mountainsImage);
+  carousel.append(treesImage);
+  carousel.append(computerImage);
+  carousel.append(turnTableImage);
+  carousel.append(rightButton);
+  // Adding Class Names
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  // text content....img content more like
+  mountainsImage.src= './assets/carousel/mountains.jpeg'
+  computerImage.src='./assets/carousel/computer.jpeg'
+  treesImage.src='./assets/carousel/trees.jpeg'
+  turnTableImage.src='./assets/carousel/turntable.jpeg'
+  
+// Event Listeners for both buttons
+
+let currentIndex =0;
+let imageArray=[mountainsImage, computerImage, treesImage, turnTableImage]
+
+  leftButton.addEventListener('click', ()=>{
+    currentIndex--
+      if(currentIndex===0){
+        currentIndex=imageArray.length;
+    }
+    
+    
+})
+
+  rightButton.addEventListener('click',()=>{
+    currentIndex++
+    if(currentIndex===imageArray.length){
+      currentIndex=0;
+    }
+  })
+  return carousel;
+}
+
+const carousel= document.querySelector('.carousel-container');
+carousel.append(carouselCreator());
+
+
